f3ce9f43f21a45bcb6b23507dced14c4d7ba677f
HarderBetterFasterStronger <sfinkel@aspiration.com>
2022-07-13 18:36:10 -0700

    try

--- run-go-imports.sh
+++ run-go-imports.sh
@@ -3,7 +3,7 @@
 # Capture and print stdout, since goimports doesn't use proper exit codes
 #
 set -e -o pipefail
-go version
+goenv version
 if ! command -v goimports &> /dev/null ; then
     echo "goimports not installed or available in the PATH" >&2
     echo "please check https://pkg.go.dev/golang.org/x/tools/cmd/goimports" >&2
